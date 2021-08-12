30c5bdfa76d76dcfbda74936e64dd56180449549
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-12 22:18:57 +0200

    removed commented code

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,3 @@
-// import React from 'react'
 import '../App.css';
 import './HeroSection.css';
 import React, { Component } from 'react'
@@ -18,28 +17,14 @@ export default class HeroSection extends Component {
         return (
             <div className='hero-container'>
                 {background}
-                {/* <video src="/intro.mp4" autoPlay loop muted /> */}
+                
 
                 <h1 class="entry-title">{this.props.title}</h1>
                 
                 <p>{this.props.paragraph}</p>
-                {/* <img src="/img-53.jpeg" /> */}
+                
             </div>
         )
     }
 }
 
-// function HeroSection() {
-
-//     return (
-//         <div className='hero-container'>
-//             <video src="/intro.mp4" autoPlay loop muted />
-//             <h1>THE REVELATION BEGINS</h1>
-//                 <p>Archangel Gabriel meets Prophet Muhammad. 
-//                     Prophet Muhammad runs to his wife Khadija bint Khwaylid 
-//                     who introduces him to Waraqa ibn Nawfal, a hanif adhering to the religion of Abraham.</p>
-//         </div>
-//     )
-// }
-
-// export default HeroSection
