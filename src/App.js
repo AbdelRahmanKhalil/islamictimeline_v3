311f57acfa15f4d4593b816877788beb48c6d69f
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-21 11:27:09 +0200

    changed intro vid to img

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
     <HeroSection title="THE REVELATION BEGINS" paragraph="Archangel Gabriel meets Prophet Muhammad.
                     Prophet Muhammad runs to his wife Khadija bint Khwaylid
-                    who introduces him to Waraqa ibn Nawfal, a hanif adhering to the religion of Abraham."  video="/intro.mp4"/>
+                    who introduces him to Waraqa ibn Nawfal, a hanif adhering to the religion of Abraham."  image="/img-0.jpg"/>
     <HeroSection title="First Islamic Society" paragraph="Prophet Muhammad goes on Hegira (departure from Mecca to Medina). 
     He's welcomed by the people of Medina. The event marks the birth of the Islamic Society." image="/img-2.jpg"/>
     <HeroSection title="Rashidun Caliphate Starts" paragraph="Prophet Mohammed dies having conqured Mecca. Most tribes of Arabia are Muslims." image="/img-3.jpg"/>
